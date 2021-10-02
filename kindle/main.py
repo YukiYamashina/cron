08466a8deffd76c1e0d43849251cb5ab8f9e6347
Yuki Yamashina <yuki@yukiyuriweb.com>
2021-10-02 22:36:19 +0900

    first commit

---
+++ kindle/main.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pprint
+import requests
+from time import sleep
+from datetime import datetime
+from pytz import timezone
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from feedgen.feed import FeedGenerator
+
+jst=timezone('Japan')
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.set_page_load_timeout(30)
+
+def main():
+    url = 'https://www.amazon.co.jp//b/?ie=UTF8&node=3251934051'
+    # print(f'Start fetching {url} ...')
+
+    try:
+        driver.get(url)
+        item_selector = '#anonCarousel1 > ol > li'
+        images = list(map(lambda elm: elm.find_element_by_css_selector('img').get_attribute('src'), driver.find_elements_by_css_selector(item_selector)))
+        items = list(map(lambda elm: {
+            'href': elm.find_element_by_css_selector('a').get_attribute('href'),
+            'title': elm.find_element_by_css_selector('img').get_attribute('alt'),
+            'thumbnail': elm.find_element_by_css_selector('img').get_attribute('src'),
+        }, driver.find_elements_by_css_selector(item_selector)))
+
+        item_data = {
+            'postToken': os.environ.get('LINE_POST_TOKEN'),
+            'type': 'image',
+            'imageUrls': images
+        }
+        with open('./kindle.json', 'w') as f:
+            json.dump(items, f, indent=4, ensure_ascii=False)
+
+        requests.post(os.environ.get('LINE_POST_API_URL'), json=item_data)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
---
+++ toushiru/main.py
@@ -0,0 +1,62 @@
+import json
+import pprint
+from time import sleep
+from datetime import datetime
+from pytz import timezone
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from feedgen.feed import FeedGenerator
+
+jst=timezone('Japan')
+
+author = '楽天証券の投資情報メディア トウシル'
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.set_page_load_timeout(30)
+
+def main():
+    url = 'https://media.rakuten-sec.net/category/toushiru'
+    print(f'Start fetching {url} ...')
+
+    try:
+        driver.get(url)
+
+        fg = FeedGenerator()
+        fg.id(url)
+        fg.title(driver.title)
+        fg.author( {'name': driver.find_element_by_xpath('/html/head/meta[13]').get_attribute('content')} )
+        fg.link( href = url, rel='alternate' )
+        fg.logo(driver.find_element_by_xpath('/html/head/meta[10]').get_attribute('content'))
+
+        articles = list(map(lambda elm: {
+            'href': elm.find_element_by_css_selector('.title a').get_attribute('href'),
+            'title': elm.find_element_by_css_selector('.title').text,
+            'category': elm.find_element_by_css_selector('.cname').text,
+            'timestamp': elm.find_element_by_css_selector('.date').text.replace('NEW ', ''),
+            'eyecatch': elm.find_element_by_css_selector('img').get_attribute('data-src'),
+            'description': elm.find_element_by_css_selector('img').get_attribute('alt')
+        }, driver.find_elements_by_css_selector('.article-list .list')))
+
+        for article in articles:
+            fe = fg.add_entry()
+            fe.id(article['href'])
+            fe.title(article['title'])
+            fe.author({ 'name': author })
+            fe.link(href=article['href'], rel='alternate')
+            fe.description(article['description'])
+            fe.published(datetime.strptime(article['timestamp'], '%Y/%m/%d').replace(tzinfo=jst))
+
+        atomfeed = fg.atom_str(pretty=True)
+        print(atomfeed)
+        fg.atom_file('./toushiru.xml')
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
